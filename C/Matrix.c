b1c6df5cf6c2b3c3dfc29aacc30eb1b318b5ab2b
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2022-08-02 17:11:08 +0530

    added

---
+++ C/Matrix.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int no[3][3];   
+    int i,j;
+    printf("Enter value:");
+    for ( i = 0; i < 3; i++)
+    {
+        /* code */
+        for ( j = 0; j < 3; j++)
+        {
+            /* code */
+            printf("value[%d][%d]:",i,j);
+            scanf("%d",&no[i][j]);
+        }
+    }
+    printf("\n\n");
+    for ( i = 0; i < 3; i++)
+    {
+        /* code */
+        for ( j = 0; j < 3; j++)
+        {
+            /* code */
+            printf("%d ",no[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+}
