8f1a8ab5d940072faa840e9e90bd1a1e7413f92d
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2022-08-30 16:25:57 +0530

    add

--- C/Array_with_Structure.c
+++ C/Array_with_Structure.c
@@ -1,1 +1,22 @@
-#
+#include<stdio.h>
+struct stdata
+{
+    int id;
+    char nm[20];
+}st[10];
+void main()
+{
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        printf("\nEnter student id:");
+        scanf("%d",&st[i].id);
+        pritnf("\nEnter student name:");
+        scanf("%s",&st[i].nm);
+    }
+    for(i = 0; i < 5; i++)
+    {
+        pritnf("\n%d Student id is:%d",i+1,st[i].id);
+        printf("\n%d Student name is:%s",i+1,st[i].nm);
+    }
+}
