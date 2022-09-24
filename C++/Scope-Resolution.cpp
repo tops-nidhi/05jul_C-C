6704986f0375e96c6723e0cec7845174353436ac
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2022-09-24 15:44:41 +0530

    commit

---
+++ C++/Scope-Resolution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int c=100; //global
+int add(int a, int b)
+{
+    int c;
+    c=a+b;
+    // cout<<endl;
+    cout<<"Addition(global) is:"<<::c<<endl;
+    return c;//local 
+    
+}
+int main()
+{
+    cout<<"\n"<<add(12,23);
+}
